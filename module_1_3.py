e29547e07fe8273fd29c9f38c5f25e109ffc7541
LiHarviK <steve2002minecraft@gmail.com>
2024-10-30 20:07:55 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -4,8 +4,8 @@ print('Name:', name)
 age = 22
 print('Age:', age)
 
-age = 22 + 1
-print('New Age:', age)
+new_age = 23
+print('New Age:', new_age)
 
 is_student = True
 print('Is student:', is_student)
